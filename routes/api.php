7424a0bf0af9e44b40bfc4b7111678dfe47bca42
Alireza Nori <alireza.nr1378@gmail.com>
2021-06-11 13:01:32 +0430

    define authentication routes.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function ()
+{
+    Route::post('login', [ AuthController::class, 'login' ]);
+    Route::post('signup', [ AuthController::class, 'signup' ]);
+});
+
+Route::group([ 'middleware' => 'auth:api' ], function ()
+{
+    Route::get('access_check', [ AuthController::class, 'index' ]);
+    Route::post('logout', [ AuthController::class, 'logout' ]);
 });
